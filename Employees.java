10d55b76d7d39a686d5cc207ed4d1057b8f95d1b
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2024-11-06 12:24:05 +0000

    Add files via upload

--- Employees.java
+++ Employees.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Employees{
     // List of employees
-    ArrayList<Employee> ListOfEmployees = new ArrayList<Employee>();
+    List<Employee> listOfEmployees = new ArrayList<>();
 
     // Method adds employee to the list, is called in the Employee class
     public void addEmployee(Employee employee){
-        ListOfEmployees.add(employee);
+        if(listOfEmployees.contains(employee)){
+            System.out.println("Employee already exists.");
+        }else{
+            listOfEmployees.add(employee);
+        }
+    }
+
+    public void removeEmployee(Employee employee){
+        listOfEmployees.remove(employee);
+    }
+
+    public List<Employee> getListOfEmployees(){
+        return listOfEmployees;
     }
 }
