e1b0bcccf3ca0e24607b52b407a6ae7586f40791
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2024-11-12 08:59:04 +0000

    Add files via upload

--- PartTime.java
+++ PartTime.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class PartTime extends Employee{
     private double hours_Per_Week;
     
-    public PartTime(String first_Name, String last_Name, String email, String phone_Number, double hours_Per_Week){
+    public PartTime(String first_Name, String last_Name, String email, String phone_Number, double hours_Per_Week, boolean validateFormFilled){
         super(first_Name, last_Name, email, phone_Number);
         this.hours_Per_Week = hours_Per_Week;
+        this.validateFormFilled = validateFormFilled();
     }
 
     
