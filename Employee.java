86243a1590f6df37378d280bc7a9feab5e59d0ad
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2024-11-14 11:39:37 +0000

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -4,6 +4,34 @@ public class Employee extends UserTypes{
         super(first_Name, last_Name, employee_ID, phone_Number, email, current_Points, marital_Status, employees);
     }
 
+    public String getFirst_Name(){
+        return first_Name;
+    }
+
+    public Boolean getMarital_Status(){
+        return marital_Status;
+    }
+
+    public String getLast_Name(){
+        return last_Name;
+    }
+
+    public int getEmployee_ID(){
+        return employee_ID;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhone_Number(){
+        return phone_Number;
+    }
+
+    public int getCurrent_Points(){
+        return current_Points;
+    }
+
     @Override
     public boolean AccessEmployeeList(){
         return false;
